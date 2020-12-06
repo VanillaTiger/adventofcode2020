4283970b73991d2e3221e2f4b074463f0f1dcf6c
Adam <a.szummer@gmail.com>
2020-12-06 19:18:53 +0100

    exercise 6 first input not nice

---
+++ exercise_6.py
@@ -0,0 +1,36 @@
+with open("exercise_6_all.txt",'r') as f:
+    lines = f.readlines()
+
+print(lines)
+person = []
+group = []
+compare = []
+sum = 0
+
+for item in lines:
+    # print(item)
+    if item=="\n":
+        # print("group=",len(group))
+        if len(group) == 1:
+            compare = set(group[0])
+        else:
+            for i in range(0,len(group)):
+                # print(i)
+                if i==0:
+                    compare = set(group[0])
+                else:
+                    compare = set(compare)&set(group[i])
+        sum+=len(set(compare))
+        # print("compare=",compare)
+        # print("sum=",sum)
+        # print(sum)
+        group=[]
+        compare=[]
+    else:
+        item = item[:-1]
+        person=[]
+        for letter in item:
+            # print(letter)
+            person.append(letter)
+        group.append(person.copy())
+print(sum)
